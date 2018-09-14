21adf8eaf1afadb09a2a335b6aa09308172d8bcf
atang90 <andrew.tang90@gmail.com>
2018-09-13 22:35:12 -0400

    Update secureerase.py

--- secureerase.py
+++ secureerase.py
@@ -1,16 +1,17 @@
 # secureerase.py
-# Andrew, 12/12/2017 (Last Revision)
-# Description of intent: Simple script to run a secure erase on a Mac OS X - in order to reclaim "purgeable space" 
+# Andrew, 09/13/2018 (Last Revision)
+# Description of intent: Simple script to run a secure erase on a Mac OS X - in order to reclaim "purgeable space"
 # NOTE:Erasing freespace only works on mounted and writable volumes
 
 import subprocess
 
 subprocess.call('df -h', shell=True)
 subprocess.call('diskutil list', shell=True)
-wone = str(input("What disk do want to secure erase?(i.e. disk0s2): "))
+wone = str(input("What disk do want to secure erase?[i.e. disk0s2]: "))
            
-ques = str(input("Do you want to secure erase freespace data in " + wone + "?(Y/N): "))
+ques = str(input(("Secure erase freespace will run on ""'" + wone + "'"", do you want to continue?(Y/N): "))
 if ques in ["Y","y"]:
+    print("diskutil secureerase freespace 0 " + wone + "........")
     subprocess.call('diskutil secureerase freespace 0 ' + wone, shell=True)
     subprocess.call('df -h', shell=True)
     print('Exiting...')
